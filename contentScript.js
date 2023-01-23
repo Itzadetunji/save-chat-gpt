2f7e37ef32b1f838a46947e56ea9651b12ce410f
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2023-01-23 20:23:26 +0100

    Feat: Added message when not on youtube, saved bookmarks to the chrome local storage

--- contentScript.js
+++ contentScript.js
@@ -1,6 +1,7 @@
 (() => {
 	let youtubeLeftControls, youtubePlayer;
 	let currentVideo = "";
+	let currentVideoBookmarks = [];
 
 	chrome.runtime.onMessage.addListener((obj, sender, response) => {
 		const { type, value, videoId } = obj;
@@ -10,9 +11,18 @@
 		}
 	});
 
-	const newVideoLoaded = () => {
+	const fetchBookmarks = () => {
+		return new Prommise((resolve) => {
+			chrome.storage.sync.get([currentVideo], (obj) => {
+				resolve(obj[currentVideo] ? JSON.parse(obj(currentVideo)) : []);
+			});
+		});
+	};
+
+	const newVideoLoaded = async () => {
 		const bookmarkBtnExists =
 			document.getElementsByClassName("bookmark-btn")[0];
+		currentVideoBookmarks = await fetchBookmarks();
 		if (!bookmarkBtnExists) {
 			const bookmarkBtn = document.createElement("img");
 
@@ -25,8 +35,31 @@
 			youtubePlayer = document.getElementsByClassName("video-stream")[0];
 
 			youtubeLeftControls.appendChild(bookmarkBtn);
+			bookmarkBtn.addEventListener("click", addNewBookmarkEventhandler);
 		}
+	};
+
+	const addNewBookmarkEventhandler = async () => {
+		const currentTime = youtubePlayer.currentTime;
+		const newBookmark = {
+			time: currentTime,
+			desc: "Bookmark at " + getTime(currentTime),
+		};
+
+		currentVideoBookmarks = await fetchBookmarks();
+
+		chrome.storage.sync.set({
+			[currentVideo]: JSON.stringify(
+				[...currentVideoBookmarks, newBookmark].sort((a, b) => a.time - b.time)
+			),
+		});
 	};
 
 	newVideoLoaded();
 })();
+
+const getTime = (t) => {
+	var date = new Date(0);
+	date.setSeconds(t);
+	return date.toISOString().substring(11, 8);
+};
